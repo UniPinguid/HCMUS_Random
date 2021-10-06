2b7f0f2e34d37a77a4b386498afb4cb5f10f6fe7
Nguyễn Hữu Khôi <65937294+khoihuu0301@users.noreply.github.com>
2021-10-07 01:04:28 +0700

    Update Cau1.sql

--- Cau1.sql
+++ Cau1.sql
@@ -4,7 +4,7 @@ USE QuanLyBanHang
 GO
 
 CREATE TABLE KhachHang (
-	makH CHAR(7),
+	makH CHAR(7) NOT NULL,
 	Ho NVARCHAR(6),
 	Ten NVARCHAR(6),
 	Ngsinh DATETIME,
@@ -17,15 +17,15 @@ CREATE TABLE KhachHang (
 )
 GO
 CREATE TABLE HoaDon (
-	MaHD CHAR(7),
+	MaHD CHAR(7) not null,
 	MaKH CHAR(7),
 	NgayLap DATETIME,
 	TongTien FLOAT,
 )
 GO
 CREATE TABLE CT_HoaDon (
-	MaHD CHAR(7),
-	MaSP CHAR(5),
+	MaHD CHAR(7) not null,
+	MaSP CHAR(5) not null,
 	SoLuong INT,
 	GiaBan FLOAT,
 	GiaGiam FLOAT,
@@ -33,10 +33,38 @@ CREATE TABLE CT_HoaDon (
 )
 GO
 CREATE TABLE SanPham (
-	MaSP CHAR(5),
+	MaSP CHAR(5) not null,
 	TenSP NVARCHAR(50),
-	SoLuongTon INT,
+	SoLuongTon INT, 
 	Mota NVARCHAR(50),
 	Gia FLOAT
 )
 
+
+go
+alter table KhachHang
+add constraint makH_pk primary key (makH)
+
+go
+alter table Hoadon
+add constraint mahd_pk primary key (mahd)
+
+go
+alter table ct_hoadon
+add constraint hdsp_pk primary key(mahd,masp)
+
+go
+alter table sanpham
+add constraint masp_pk primary key (masp)
+
+go
+alter table Hoadon
+add constraint hd_makh_fk foreign key (makh) references khachhang(makh)
+
+go
+alter table ct_hoadon
+add constraint ct_hoadon_hoadon_fk foreign key (mahd) references hoadon(mahd)
+
+go
+alter table ct_hoadon
+add constraint ct_hoadon_sp_fk foreign key (masp) references sanpham(masp)
