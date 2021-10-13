897ddda95277bf9e8360a01c29f66afb0aeb0664
Lê Bảo Chấn Phát <31151363+UniPinguid@users.noreply.github.com>
2021-10-13 17:55:59 +0700

    Add files via upload

---
+++ ui/App/QuanLyBanHangApp/QuanLyBanHangApp/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyBanHangApp
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source=DELL;Initial Catalog=QuanLyBanHang;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection SqlCon = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM HoaDon where MaHD like '%'+@MaHD+'%'", SqlCon))
+                {
+                    cmd.Parameters.AddWithValue("MaHD", textBox1.Text);
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                    DataTable dtbl = new DataTable("HoaDon");
+                    adapter.Fill(dtbl);
+                    dataGridView1.DataSource = dtbl;
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
