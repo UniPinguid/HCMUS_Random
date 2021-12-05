f621d5352f841b51dbe66749356e51d0b77ef95d
TungPham24 <tung38085@gmail.com>
2021-12-05 20:40:39 +0700

    Them loi 5 6

---
+++ Concurrency problems/19120423/01-Unrepeatable Read/SuaSanPham_1.sql
@@ -0,0 +1,2 @@
+DECLARE @idSanPham CHAR(8), @gia INT
+EXEC SuaSanPham @idSanPham, @gia
---
+++ Concurrency problems/19120423/02-Unrepeatable Read/CapNhatTenDoiTac.sql
@@ -0,0 +1,7 @@
+USE ONLINE_STORE
+GO
+
+DECLARE @idDoiTac CHAR(8), @newname NVARCHAR(30)
+
+EXEC CapNhatTenDoiTac @idDoiTac, @newname
+
