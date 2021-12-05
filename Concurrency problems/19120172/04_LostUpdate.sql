732bc30ed60aef069257544fd64dbd3b2fce96db
nsonbao261 <91799283+nsonbao261@users.noreply.github.com>
2021-12-05 21:23:57 +0700

    Add files via upload

--- Concurrency problems/19120172/04_LostUpdate.sql
+++ Concurrency problems/19120172/04_LostUpdate.sql
@@ -1,1 +1,54 @@
+﻿create procedure sp_CapNhatTinhTrang
+@IDDonHang char(10),
+@status int
+as
+BEGIN TRAN
+	if not exists (select * from DONHANG where DonHangID = @IDDonHang )
+	begin
+		print N'Đơn hàng không tồn tại'
+		rollback tran
+	end
+	waitfor delay '00:00:20'
+	update DONHANG
+	set TinhTrang = @status
+	where DonHangID = @IDDonHang
+	if (@@ERROR<>0)
+	begin
+		rollback tran
+		print N'Cập nhật thất bại'
+	end
+	else
+	begin
+		commit tran 
+		print N'Cập nhật thành công'
+	end
+go
+
+
+create procedure sp_CapNhatTinhTrang_2
+@IDDonHang char(10),
+@status int
+as
+BEGIN TRAN
+	if not exists (select * from DONHANG where DonHangID = @IDDonHang )
+	begin
+		print N'Đơn hàng không tồn tại'
+		rollback tran
+	end
+	update DONHANG
+	set TinhTrang = @status
+	where DonHangID = @IDDonHang
+	if (@@ERROR<>0)
+	begin
+		rollback tran
+		print N'Cập nhật thất bại'
+	end
+	else
+	begin
+		commit tran 
+		print N'Cập nhật thành công'
+	end
+go
+
+
 
